5ad4edf89ca0b9098a8b7ab2c3f2a1b3b2bc5df1
santosh <santoshgurkhe602@gmail.com>
2026-03-12 10:54:14 +0530

    Add collectstatic

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -83,3 +83,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://pmax-cinema-production.up.railway.app',
+    'http://pmax-cinema-production.up.railway.app',
+]
